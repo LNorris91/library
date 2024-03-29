e37773ac8292301e1312fb42b8d2e7f31146c559
Luke Norris <154083568+LNorris91@users.noreply.github.com>
2024-03-29 09:05:49 -0400

    Create Function to Display Library on Screen
    this commit adds javascript that will create
    HTML elements for the books and display their
    info on the screen in the form of cards.

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+const libraryContainer = document.querySelector(".libraryContainer");
+
 const myLibrary = [];
 
 function Book(title, author, pages, isRead) {
@@ -7,10 +9,32 @@ function Book(title, author, pages, isRead) {
     this.read = Boolean(isRead);
 }
 
+const theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', '287', true);
+myLibrary.push(theHobbit)
+const deathOnTheNile = new Book('Death on the Nile', 'Agatha Christie', '288', true);
+myLibrary.push(deathOnTheNile)
+
 function addBookToLibrary(newBook) {
     myLibrary.push(newBook)
 }
 
-const theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', '287', true);
+function addLibraryToPage() {
+    myLibrary.forEach(book => {
+        const bookCard = document.createElement('div');
+        bookCard.classList.add('bookCard');
 
-addBookToLibrary(theHobbit)
+        const bookTitle = document.createElement('div');
+        const bookAuthor = document.createElement('div');
+        const bookPages = document.createElement('div');
+        const bookRead = document.createElement('div');
+        
+        bookTitle.textContent = (`Title: ${book.title}`);
+        bookAuthor.textContent = (`Author: ${book.author}`);
+        bookPages.textContent = (`Number of Pages: ${book.pages}`);
+        bookRead.textContent = (`Been Read? ${book.read}`);
+
+        bookCard.append(bookTitle,bookAuthor,bookPages,bookRead);
+        libraryContainer.appendChild(bookCard);
+})}
+
+addLibraryToPage()
