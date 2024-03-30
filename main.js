e0f14bd0f9be4068fa0cc599cc4bc6b32234437d
Luke Norris <154083568+LNorris91@users.noreply.github.com>
2024-03-30 11:18:24 -0400

    Adds Functionality to Card Buttons
    this commit creates functions for the book remove
    button as well as the read status button

--- main.js
+++ main.js
@@ -1,14 +1,15 @@
 const libraryContainer = document.querySelector(".libraryContainer");
 const dialog = document.querySelector("dialog");
+// const removeBtn = document.querySelector(".removeBtn")
 
 const myLibrary = [];
 let counter = 0;
 
-function Book(title, author, pages, isRead) {
+function Book(title, author, pages, read) {
     this.title = title;
     this.author = author;
     this.pages = pages;
-    this.read = Boolean(isRead);
+    this.read = read;
 }
 
 const theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', '287', true);
@@ -23,7 +24,7 @@ function addBookToLibrary() {
         document.getElementById("title").value,
         document.getElementById("author").value,
         document.getElementById("pages").value,
-        document.getElementById("read").value
+        document.getElementById("read").checked
     )
     dialog.close();
     myLibrary.push(newBook);
@@ -33,21 +34,52 @@ function addBookToLibrary() {
 function addLibraryToPage() {
     myLibrary.forEach(book => {
         if (myLibrary.indexOf(book) >= counter) {
-        const bookCard = document.createElement('div');
-        bookCard.classList.add('bookCard');
 
+        const bookCard = document.createElement('div');
+              bookCard.classList.add('bookCard');
         const bookTitle = document.createElement('div');
         const bookAuthor = document.createElement('div');
         const bookPages = document.createElement('div');
         const bookRead = document.createElement('div');
+        const removeBtn = document.createElement('button');
+              removeBtn.classList.add('removeBtn');
+              removeBtn.addEventListener('click', remove)
+        const readStatus = document.createElement('button');
+              readStatus.classList.add('readStatus');
+              readStatus.addEventListener('click', readUpdate);
         
         bookTitle.textContent = (`Title: ${book.title}`);
         bookAuthor.textContent = (`Author: ${book.author}`);
         bookPages.textContent = (`Number of Pages: ${book.pages}`);
-        bookRead.textContent = (`Been Read? ${book.read}`);
+        if (book.read == true) {
+            bookRead.textContent = ('Read');
+        } else {
+            bookRead.textContent = ("Not Read");
+        }
+        removeBtn.textContent = ("X");
+        readStatus.textContent = ("read it yet?");
 
-        bookCard.append(bookTitle,bookAuthor,bookPages,bookRead);
+        bookCard.append(bookTitle,bookAuthor,bookPages,bookRead,removeBtn,readStatus);
         libraryContainer.appendChild(bookCard);
+
+        function remove() {
+            const x = (myLibrary.indexOf(book));
+            libraryContainer.removeChild(bookCard)
+            myLibrary.splice(x, 1);
+            counter--
+        }
+
+        function readUpdate() {
+            if (book.read === true) {
+                book.read = false;
+                bookRead.textContent = "Not Read"
+            } else {
+                book.read = true;
+                bookRead.textContent = "Read"
+            }
+        }
+
         counter++
 }})}
+
 
