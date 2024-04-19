7a8ea487a74b3bca323519d5ab1b788252c8e7d4
Luke Norris <154083568+LNorris91@users.noreply.github.com>
2024-04-19 11:05:26 -0400

    Switches Constructor to Class Format
    this commit just swaps out my object Constructor
    for a Class Constructor. everything still
    functions and operates the same as before.

--- main.js
+++ main.js
@@ -4,11 +4,13 @@ const dialog = document.querySelector("dialog");
 const myLibrary = [];
 let counter = 0;
 
-function Book(title, author, pages, read) {
+class Book {
+    constructor(title, author, pages, read) {
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.read = read;
+    }
 }
 
 const theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', '287', true);
